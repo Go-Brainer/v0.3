6474a034df63ecd3521941b091471169fcc5803a
AlexDVidal <avidal@csu.fullerton.edu>
2019-10-02 05:25:04 -0700

    Implemented the configurable Go-Brainer script and more robust human io.

--- Go-Brainer.py
+++ Go-Brainer.py
@@ -4,7 +4,10 @@ from dlgo import gotypes
 from dlgo.utils import print_board, print_move, point_from_coords
 from six.moves import input
 from os import name, system
+from dlgo.scoring import compute_game_result
 from re import match
+from sys import argv
+import time
 
 
 def clear():
@@ -14,10 +17,44 @@ def clear():
         _ = system('clear')
 
 
+def print_usage():
+    print("Usage: python3 Go-Brainer.py <5-19> <hb|bh|bb>")
+
+
 def main():
-    board_size = 9
-    game = goboard.GameState.new_game(board_size)
-    bot = naive.RandomBot()
+    if len(argv) is not 3:
+        print_usage()
+        exit(0)
+    if not argv[1].isnumeric() or \
+            int(argv[1]) < 5 or \
+            int(argv[1]) > 19:
+        print_usage()
+        exit(0)
+
+    game = goboard.GameState.new_game(int(argv[1]))
+
+    human = None
+    players = {}
+    p = argv[2].lower()
+    if p == "hb":
+        players = {
+            gotypes.Player.black: human,
+            gotypes.Player.white: naive.RandomBot()
+        }
+    elif p == "bh":
+        players = {
+            gotypes.Player.black: naive.RandomBot(),
+            gotypes.Player.white: human
+        }
+    elif p == "bb":
+        players = {
+            gotypes.Player.black: naive.RandomBot(),
+            gotypes.Player.white: naive.RandomBot()
+        }
+    else:
+        print_usage()
+        exit(0)
+
     print_board(game.board)
 
     while not game.is_over():
@@ -25,7 +62,7 @@ def main():
         # next player we must save the current player
         player_before = game.next_player
         move = None
-        if game.next_player == gotypes.Player.black:
+        if players[game.next_player] == human:
             valid_move = False
             while not valid_move:
                 try:
@@ -50,11 +87,18 @@ def main():
                     print("Invalid move")
             # end of human input loop
         else:
-            move = bot.select_move(game)
+            move = players[game.next_player].select_move(game)
         clear()
         game = game.apply_move(move)
         print_board(game.board)
         print_move(player_before, move)
+        time.sleep(.3)
+    # end of main game loop
+
+    results = compute_game_result(game)
+    print("White Final Score: %d" % results.w)
+    print("Black Final Score: %.1f" % (results.b - results.komi))
+    print(results.winner, "wins!")
 
 
 if __name__ == '__main__':
