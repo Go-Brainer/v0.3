1dfdb4c893b779d2842442289af7017f23249af7
Nick Bernstein <skolyr@csu.fullerton.edu>
2019-10-20 12:13:42 -0700

    Added goboard_fast
    Needed to include None option for zobrist hash and add MoveAge to utils

--- dlgo/utils.py
+++ dlgo/utils.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-
+import numpy as np
+# tag::print_utils[]
 from dlgo import gotypes
 
-# 19 columns of the board, skipping 'I'
 COLS = 'ABCDEFGHJKLMNOPQRST'
-
-# Dictionary for drawing an ascii board
 STONE_TO_CHAR = {
-        None: ' . ',
-        gotypes.Player.black: ' x ',
-        gotypes.Player.white: ' o ',
-        }
+    None: ' . ',
+    gotypes.Player.black: ' x ',
+    gotypes.Player.white: ' o ',
+}
 
 
 def print_move(player, move):
@@ -32,9 +29,39 @@ def print_board(board):
             line.append(STONE_TO_CHAR[stone])
         print('%s%d %s' % (bump, row, ''.join(line)))
     print('    ' + '  '.join(COLS[:board.num_cols]))
+# end::print_utils[]
 
 
+# tag::human_coordinates[]
 def point_from_coords(coords):
     col = COLS.index(coords[0]) + 1
     row = int(coords[1:])
     return gotypes.Point(row=row, col=col)
+# end::human_coordinates[]
+
+
+def coords_from_point(point):
+    return '%s%d' % (
+        COLS[point.col - 1],
+        point.row
+    )
+
+
+# NOTE: MoveAge is only used in chapter 13, and doesn't make it to the main text.
+# This feature will only be implemented in goboard_fast.py so as not to confuse
+# readers in early chapters.
+class MoveAge():
+    def __init__(self, board):
+        self.move_ages = - np.ones((board.num_rows, board.num_cols))
+
+    def get(self, row, col):
+        return self.move_ages[row, col]
+
+    def reset_age(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = -1
+
+    def add(self, point):
+        self.move_ages[point.row - 1, point.col - 1] = 0
+
+    def increment_all(self):
+        self.move_ages[self.move_ages > -1] += 1
