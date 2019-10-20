cc1c2b956c6fb030721fd242ba669d2a715978ba
Nick Bernstein <skolyr@csu.fullerton.edu>
2019-10-20 04:22:50 -0700

    Changed player selection options to choose agents
    Changed usage so now positional arguments determine which agent is used for black and white respectively.

--- Go-Brainer.py
+++ Go-Brainer.py
@@ -1,10 +1,10 @@
 import dlgo.agent.naive as naive
+from dlgo.agent.human_agent import HumanAgent
+from dlgo.minimax.depthprune import DepthPrunedAgent
 from dlgo import goboard
 from dlgo import gotypes
-from dlgo.utils import print_board, print_move, point_from_coords
-from six.moves import input
+from dlgo.utils import print_board, print_move
 from os import name, system
-from re import match
 from sys import argv
 import time
 import argparse
@@ -17,12 +17,31 @@ def clear():
         _ = system('clear')
 
 
+def capture_diff(game_state):
+    black_stones = 0
+    white_stones = 0
+    for r in range(1, game_state.board.num_rows + 1):
+        for c in range(1, game_state.board.num_cols + 1):
+            p = gotypes.Point(r, c)
+            color = game_state.board.get(p)
+            if color == gotypes.Player.black:
+                black_stones += 1
+            elif color == gotypes.Player.white:
+                white_stones += 1
+    diff = black_stones - white_stones                    # <1>
+    if game_state.next_player == gotypes.Player.black:    # <2>
+        return diff                                       # <2>
+    return -1 * diff
+
+
 def get_options(args=argv[1:]):
     parser = argparse.ArgumentParser(prog="Go-Brainer", description="An AI Go Bot")
 
-    parser.add_argument('-s', '--size', type=int, default=9, help="Board size is n by n. Must be between 5 and 19.")
-    parser.add_argument('-m', '--human', type=lambda c: c.lower(), choices=['b', 'w'],
-                        help="Enables human v. bot mode and sets human's color.")
+    parser.add_argument('b_agent', type=lambda c: c.lower(), choices=['r', 'd', 'h'],
+                        help="Choose Player Agent for black: r - random, d - depth pruned minimax, h - human")
+    parser.add_argument('w_agent', type=lambda c: c.lower(), choices=['r', 'd', 'h'],
+                        help="Choose Player Agent for black: r - random, d - depth pruned minimax, h - human")
+    parser.add_argument('-s', '--size', type=int, default=5, help="Board size is n by n. Must be between 5 and 19.")
     parser.add_argument('-k', '--komi', type=float, default=7.5, help="Sets komi. Must be between 0 and 10.")
 
     options = parser.parse_args(args)
@@ -32,7 +51,6 @@ def get_options(args=argv[1:]):
 def main():
     options = get_options(argv[1:])
     size = abs(options.size)
-    human = options.human
     komi = abs(options.komi)
 
     if size < 5 or size > 19:
@@ -44,27 +62,34 @@ def main():
         exit(-1)
 
     game = goboard.GameState.new_game(size)
-    players = {}
-    if human is None:
-        players = {
-            gotypes.Player.black: naive.RandomBot(),
-            gotypes.Player.white: naive.RandomBot()
-        }
-    elif human == 'b':
-
-        players = {
-            gotypes.Player.black: human,
-            gotypes.Player.white: naive.RandomBot()
-        }
-    elif human == 'w':
-        players = {
-            gotypes.Player.black: naive.RandomBot(),
-            gotypes.Player.white: human
-        }
+    if options.b_agent == 'r':
+        b_agent = naive.RandomBot()
+    elif options.b_agent == 'd':
+        b_agent = DepthPrunedAgent(3, capture_diff)
+    elif options.b_agent == 'h':
+        b_agent = HumanAgent()
     else:
+        b_agent = None
         print(options)
         print("Invalid options error")
         exit(0)
+
+    if options.w_agent == 'r':
+        w_agent = naive.RandomBot()
+    elif options.w_agent == 'd':
+        w_agent = DepthPrunedAgent(3, capture_diff)
+    elif options.w_agent == 'h':
+        w_agent = HumanAgent()
+    else:
+        w_agent = None
+        print(options)
+        print("Invalid options error")
+        exit(0)
+
+    players = {
+        gotypes.Player.black: b_agent,
+        gotypes.Player.white: w_agent
+    }
 
     print_board(game.board)
 
@@ -72,33 +97,7 @@ def main():
         # Since making a play is necessary for changing board state but also changes
         # next player we must save the current player
         player_before = game.next_player
-        move = None
-        if players[game.next_player] == human:
-            valid_move = False
-            while not valid_move:
-                try:
-                    human_move = input('-- ').upper()
-                    if match("P(ASS)*$", human_move):
-                        move = goboard.Move.pass_turn()
-                    elif match("R(ESIGN)*$", human_move):
-                        move = goboard.Move.resign()
-                    else:
-                        point = point_from_coords(human_move.strip())
-                        move = goboard.Move.play(point)
-
-                    valid_move = game.is_valid_move(move)
-                    if not valid_move:
-                        print("Invalid move")
-
-                except AssertionError:
-                    print("Invalid move")
-                except ValueError:
-                    print("Invalid move")
-                except IndexError:
-                    print("Invalid move")
-            # end of human input loop
-        else:
-            move = players[game.next_player].select_move(game)
+        move = players[game.next_player].select_move(game)
         clear()
         game = game.apply_move(move)
         print_board(game.board)
