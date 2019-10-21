f22f37e66ae8c9b11bc049258044c4b7de36003a
Nick Bernstein <skolyr@csu.fullerton.edu>
2019-10-20 19:40:19 -0700

    Added CNN modelling scripts
    train_1 & train_1_output is with my generated game set. Others tested with 40k version for verification with textbook.

---
+++ cnn_training/train_1.py
@@ -0,0 +1,40 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+np.random.seed(123)
+
+X = np.load('../generated_games/features.npy')
+Y = np.load('../generated_games/labels.npy')
+samples = X.shape[0]
+board_size = 9 * 9
+
+X = X.reshape(samples, board_size)  # <3>
+Y = Y.reshape(samples, board_size)
+
+train_samples = int(0.9*samples)
+X_train, X_test = X[:train_samples], X[train_samples:]
+Y_train, Y_test = Y[:train_samples], Y[train_samples:]
+# end::mcts_go_mlp_preprocessing[]
+
+# tag::mcts_go_mlp_model[]
+model = Sequential()
+model.add(Dense(1000, activation='sigmoid', input_shape=(board_size,)))
+model.add(Dense(500, activation='sigmoid'))
+model.add(Dense(board_size, activation='sigmoid'))
+model.summary()
+
+model.compile(loss='mean_squared_error',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.fit(X_train, Y_train,
+          batch_size=64,
+          epochs=5,
+          verbose=1,
+          validation_data=(X_test, Y_test))
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+# end::mcts_go_mlp_model[]
